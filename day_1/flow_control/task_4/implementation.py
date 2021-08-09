d84745192a49bbe30c7a1bd4c9b296dd750de5aa
zikfood <panda007a@mail.ru>
2021-08-09 21:31:14 +0300

    day 1, task 4 refactoring

--- day_1/flow_control/task_4/implementation.py
+++ day_1/flow_control/task_4/implementation.py
@@ -6,24 +6,27 @@ def get_next_date(some_date):
     #return some_date + timedelta(1)
     long_months = (1, 3, 5, 7, 8, 10, 12)
     short_months = (4, 6, 9, 11)
+    day = some_date.day
+    month = some_date.month
+    year = some_date.year
 
-    if some_date.day == 31:
-        if some_date.month == 12:
-            result = date(some_date.year + 1, 1, 1)
-        elif some_date.month in long_months:
-            result = date(some_date.year, some_date.month + 1, 1)
+    if day == 31:
+        if month == 12:
+            result = date(year + 1, 1, 1)
+        elif month in long_months:
+            result = date(year, month + 1, 1)
 
-    elif some_date.day == 30 and some_date.month in short_months:
-        result = date(some_date.year, some_date.month + 1, 1)
+    elif day == 30 and month in short_months:
+        result = date(year, month + 1, 1)
 
-    elif some_date.month == 2:
-        if some_date.day == 29:
-            result = date(some_date.year, 3, 1)
-        elif some_date.day == 28 and (some_date.year % 4 == 0 and some_date.year % 100 != 0):
-            result = date(some_date.year, 2, 29)
+    elif month == 2:
+        if day == 29:
+            result = date(year, 3, 1)
+        elif day == 28 and (year % 4 == 0 and year % 100 != 0):
+            result = date(year, 2, 29)
 
     else:
-        result = date(some_date.year, some_date.month, some_date.day + 1)
+        result = date(year, month, day + 1)
 
     return result
 
