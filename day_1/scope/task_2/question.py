672c5fb8ae2e22f7c07a4a7338126b28ad7efffc
zikfood <panda007a@mail.ru>
2021-08-09 21:27:35 +0300

    day 1 scope questions

--- day_1/scope/task_2/question.py
+++ day_1/scope/task_2/question.py
@@ -12,3 +12,9 @@ def transmit_to_space(message):
 
 
 print(transmit_to_space("Test message"))
+
+"""
+Test message и None. Сначала мы вызываем функцию transmit_to_space, 
+в которой инициализируется внутренняя функция и выдает Test message. После чего, принт, который вызывал transmit_to_space
+выдает null, потому что transmit_to_space ри выполнении ничего не возвращает
+"""
